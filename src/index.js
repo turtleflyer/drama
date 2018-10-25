69e57ad1ad77afde349401596ecb0ca185dd05e5
turtleflyer <turtlefly@gmail.com>
2018-10-25 00:34:47 -0400

    folders structure changed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './styles.css';
 import './role_sets/stage/stage';
 import './screens/gameplay/role_sets/mugsOnLine/mugsOnLine';
-import './screens/gameplay/supersets/setA';
+import './screens/gameplay/supersets/setA/setA';
 import sendPulse from './role_sets/stage/roles/sendPulse';
 import startStopLevel from './screens/gameplay/roles_manipulators/start_stop_level';
 
--- src/screens/gameplay/supersets/setA.js
+++
@@ -1,4 +0,0 @@
-import { ActorsSet } from '../../../actors_and_roles';
-import mugs from './mugs/mugs';
-
-export default new ActorsSet([mugs]);
