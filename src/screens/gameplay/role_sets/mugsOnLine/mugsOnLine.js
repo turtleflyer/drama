2e2773a7aa9371a936ff26ffce727984e6862ae2
turtleflyer <turtlefly@gmail.com>
2018-10-30 01:15:46 -0400

    Export line changed

--- src/screens/gameplay/role_sets/mugsOnLine/mugsOnLine.js
+++ src/screens/gameplay/role_sets/mugsOnLine/mugsOnLine.js
@@ -2,9 +2,9 @@
 import { ActorsSet } from '../../../../libs/actors_and_roles';
 
 export const signalElementAmongMugs = Symbol('@@mugsOnLine/signalElementAmongMugs');
-const mugsOnLine = new ActorsSet();
+
+export const mugsOnLine = new ActorsSet();
+
 mugsOnLine.getInitializer(function () {
   this.addElement(signalElementAmongMugs);
 });
-
-export { mugsOnLine };
