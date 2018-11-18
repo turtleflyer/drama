401d1efae04471eaa02d7c3c3fb0985559d72cde
turtleflyer <turtlefly@gmail.com>
2018-11-18 17:41:04 -0500

    Overfilling issue when faucet openning for short time fixed

--- src/debug/displayInfo.js
+++ src/debug/displayInfo.js
@@ -2,6 +2,7 @@
 import { ActorsSet } from '../libs/actors_and_roles';
 import stage from '../role_sets/stage/stage';
 import { onPulseTick } from '../assets/role_classes';
+import { fillingMugs } from '../screens/gameplay/role_sets/fillingMugs';
 
 const info = document.createElement('div');
 document.querySelector('body').appendChild(info);
@@ -12,7 +13,10 @@ export default infoSet;
 onPulseTick
   .registerAction(infoSet, {
     action() {
-      const textToDisplay = JSON.stringify(stage.state);
+      const textToDisplay = `${JSON.stringify(stage.state)}\r\n${[...fillingMugs].reduce(
+        (str, mug) => `${str}\r\n${JSON.stringify(mug.state, ['total', 'overfilled', 'overfilledPhase'])}`,
+        '',
+      )}`;
       info.innerText = textToDisplay;
     },
   })
--- src/screens/gameplay/role_sets/fillingMugs.js
+++ src/screens/gameplay/role_sets/fillingMugs.js
@@ -12,43 +12,42 @@ export const fillMugsRole = onPulseTick.registerAction(fillingMugs, {
       const { faucet } = mug.state;
       if (faucet.state.isOpened) {
         const currTime = Date.now();
-        if (mug.state.lastTime) {
-          if (mug.state.overfilled) {
-            if (mug.state.whenSwitchNextOverfilledPhase) {
-              if (currTime > mug.state.whenSwitchNextOverfilledPhase) {
-                // prettier-ignore
-                mug.state.overfilledPhase = (mug.state.overfilledPhase + 1)
-                % mug.params.numberOfOverfilledPhases;
-                mug.state.whenSwitchNextOverfilledPhase += mug.params.overfilledAnimationPhaseTime;
-                mug.updateFillRepresentation();
-              }
-            } else {
+        if (mug.state.overfilled) {
+          if (mug.state.whenSwitchNextOverfilledPhase) {
+            if (currTime > mug.state.whenSwitchNextOverfilledPhase) {
               // prettier-ignore
-              mug.state.whenSwitchNextOverfilledPhase = currTime
-              + mug.params.overfilledAnimationPhaseTime;
+              mug.state.overfilledPhase = (mug.state.overfilledPhase + 1)
+                % mug.params.numberOfOverfilledPhases;
+              mug.state.whenSwitchNextOverfilledPhase += mug.params.overfilledAnimationPhaseTime;
+              mug.updateFillRepresentation();
             }
           } else {
-            let beerQuantity = (currTime - mug.state.lastTime) / 1000 / mug.params.volume;
-            const overfilledQuantity = mug.state.total + beerQuantity - 1;
             // prettier-ignore
-            beerQuantity = overfilledQuantity > 0
-              ? beerQuantity - overfilledQuantity : beerQuantity;
-            // prettier-ignore
-            mug.state.beers[faucet.state.beer] = (mug.state.beers[faucet.state.beer] || 0)
+            mug.state.whenSwitchNextOverfilledPhase = currTime
+            + mug.params.overfilledAnimationPhaseTime;
+            mug.updateFillRepresentation();
+          }
+        } else if (mug.state.lastTime) {
+          let beerQuantity = (currTime - mug.state.lastTime) / 1000 / mug.params.volume;
+          const overfilledQuantity = mug.state.total + beerQuantity - 1;
+          // prettier-ignore
+          beerQuantity = overfilledQuantity > 0
+            ? beerQuantity - overfilledQuantity : beerQuantity;
+          // prettier-ignore
+          mug.state.beers[faucet.state.beer] = (mug.state.beers[faucet.state.beer] || 0)
                 + beerQuantity;
-            mug.state.total += beerQuantity;
-            if (mug.state.total === 1) {
-              mug.state.overfilled = true;
-              mug.state.overfilledPhase = -1;
-              // prettier-ignore
-              mug.state.whenSwitchNextOverfilledPhase = currTime
+          mug.state.total += beerQuantity;
+          if (mug.state.total === 1) {
+            mug.state.overfilled = true;
+            mug.state.overfilledPhase = 0;
+            // prettier-ignore
+            mug.state.whenSwitchNextOverfilledPhase = currTime
                   + mug.params.overfilledAnimationPhaseTime / 2
                   + (mug.state.nextFillThreshold - 1 - overfilledQuantity)
                   / mug.params.volume * 1000;
-            } else if (mug.state.total >= mug.state.nextFillThreshold) {
-              mug.state.fillingPhase += 1;
-              mug.updateNextThreshold().updateFillRepresentation();
-            }
+          } else if (mug.state.total >= mug.state.nextFillThreshold) {
+            mug.state.fillingPhase += 1;
+            mug.updateNextThreshold().updateFillRepresentation();
           }
         }
         mug.state.lastTime = currTime;
