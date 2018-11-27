6e2091d3c178147cd287ab6665de1e0433cdc6dc
turtleflyer <turtlefly@gmail.com>
2018-11-27 01:41:22 -0500

    Revert "scaleF to be a property of options object in arguments of Actor class"
    This reverts commit cbdebeed7e84afccfbbc52ac7946febd800578fb.

--- src/screens/gameplay/actor_classes/Damage.js
+++ src/screens/gameplay/actor_classes/Damage.js
@@ -5,7 +5,7 @@ import { damagesParams } from '../assets/gameplay_params';
 
 export default class Damage extends Actor {
   constructor(faucet, phase) {
-    super('div', damagesParams.position[phase], { scaleF: stage.scaleF });
+    super('div', damagesParams.position[phase], stage.scaleF);
     this.node.textContent = '-$5';
     this.getAppendedAsChild(faucet);
     this.attachClassName('damages');
