bc30198123697e45721d567a4c9a238df5c5b4f2
turtleflyer <turtlefly@gmail.com>
2018-12-19 23:29:06 -0500

    fallenMug behavior changed

--- src/screens/gameplay/role_sets/fallenMug/fallenMug.js
+++ src/screens/gameplay/role_sets/fallenMug/fallenMug.js
@@ -1,18 +1,23 @@
 /* eslint-env browser */
-import { ActorsSet } from '../../../../libs/actors_and_roles';
+import { ActorsSet, registerActionOfType } from '../../../../libs/actors_and_roles';
 import stage from '../../../../stage/stage';
 import { tuneGame } from '../../../../stage/gameplay_params';
+import './styles.css';
 
 // eslint-disable-next-line
 export const fallenMug = new ActorsSet();
 
 fallenMug.name = 'fallenMug';
 
-fallenMug.onAddActorEvent(function ({ target: mug }) {
-  mug.dropDown();
+fallenMug.onAddActorEvent(({ target: mug }) => {
+  mug.setZIndex(90);
+  mug.attachClassName('fallenMug');
   stage.state.reputation += tuneGame.reputationDecrement;
-  window.setTimeout(() => {
-    mug.node.remove();
-    this.deleteElement(mug);
-  }, 1000);
 });
+
+registerActionOfType('animationend', fallenMug, {
+  action({ roleSet, target: mug }) {
+    mug.remove();
+    roleSet.deleteElement(mug);
+  },
+}).start();
