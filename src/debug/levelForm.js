dd032c82f244479bec88f85c02451b3e7029f5d0
turtleflyer <turtlefly@gmail.com>
2018-12-30 23:49:08 -0500

    Formatting and cleaning

--- src/debug/levelForm.js
+++ src/debug/levelForm.js
@@ -1,13 +1,15 @@
 /* eslint-env browser */
 import { ActorsSet, registerActionOfType } from '../libs/actors_and_roles';
-import { startStopLevel } from '../roles_manipulators';
 import { startLevel } from '../stage/level_starter';
 
 document
   .querySelector('body')
   .insertAdjacentHTML(
     'beforeend',
-    '<form style="margin: 10px" id="LevelForm"><input type="number" name="level" value="0" style="width: 70px; height: 20px; margin-right: 10px"><input type="submit" value="Submit" style="height: 25px"></form>',
+    `<form style="margin: 10px" id="LevelForm">
+      <input type="number" name="level" value="0" style="width: 70px; height: 20px; margin-right: 10px">
+      <input type="submit" value="Submit" style="height: 25px">
+    </form>`,
   );
 
 const form = document.querySelector('#LevelForm');
@@ -18,7 +20,6 @@ registerActionOfType('submit', stopButton, {
   action({ event }) {
     event.preventDefault();
     const level = Number(event.srcElement[0].value);
-    startStopLevel.stop();
     startLevel(level);
   },
 }).start();
