7f56239e647297f52c0f72e8d6420fe7a88a16be
turtleflyer <turtlefly@gmail.com>
2019-01-12 01:33:33 -0500

    levelForm in debug module displays current level when game starts

--- src/debug/levelForm.js
+++ src/debug/levelForm.js
@@ -1,18 +1,20 @@
 /* eslint-env browser */
 import { ActorsSet, registerActionOfType } from '../libs/actors_and_roles';
 import { startLevel } from '../stage/level_starter';
+import stage from '../stage/stage';
 
-document
-  .querySelector('body')
-  .insertAdjacentHTML(
-    'beforeend',
-    `<form style="margin: 10px" id="LevelForm">
+document.querySelector('body').insertAdjacentHTML(
+  'beforeend',
+  `<form style="margin: 10px" id="LevelForm">
       <input type="number" name="level" value="0" style="width: 70px; height: 20px; margin-right: 10px">
       <input type="submit" value="Submit" style="height: 25px">
     </form>`,
-  );
+);
 
 const form = document.querySelector('#LevelForm');
+window.setTimeout(() => {
+  form.querySelector('input[type="number"').value = stage.state.level;
+}, 2000);
 
 const stopButton = new ActorsSet([{ node: form }]);
 
