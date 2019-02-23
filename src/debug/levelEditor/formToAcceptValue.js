6962c7bb9db2b2dc6042e716fdd11476e7614c34
turtleflyer <turtlefly@gmail.com>
2019-02-23 00:33:45 -0500

    Add method to create generic form for Level Editor

---
+++ src/debug/levelEditor/formToAcceptValue.js
@@ -0,0 +1,15 @@
+/* eslint-env browser */
+import './style.css';
+
+export default function formToAcceptValue(name, valueKeeper) {
+  const form = document.createElement('form');
+  form.className = 'level-value-accept-form';
+  form.innerHTML = `
+    <label>${name}&nbsp&nbsp</label>
+    <input type="number" name="${name}" ${
+  valueKeeper.value ? `value="${valueKeeper.value}"` : ''
+} class="level-value-accept-form__input">
+  `;
+
+  return form;
+}
