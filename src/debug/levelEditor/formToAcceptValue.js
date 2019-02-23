32017c64aa027015a4b0c93499f30213dfb4c96b
turtleflyer <turtlefly@gmail.com>
2019-02-23 00:39:29 -0500

    Rename style names

--- src/debug/levelEditor/formToAcceptValue.js
+++ src/debug/levelEditor/formToAcceptValue.js
@@ -3,12 +3,12 @@ import './style.css';
 
 export default function formToAcceptValue(name, valueKeeper) {
   const form = document.createElement('form');
-  form.className = 'level-value-accept-form';
+  form.className = 'level-form';
   form.innerHTML = `
     <label>${name}&nbsp&nbsp</label>
     <input type="number" name="${name}" ${
   valueKeeper.value ? `value="${valueKeeper.value}"` : ''
-} class="level-value-accept-form__input">
+} class="level-form__input">
   `;
 
   return form;
