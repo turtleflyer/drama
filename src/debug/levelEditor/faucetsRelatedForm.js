b0bb4a0059b1e3714fbf415bc4b13daee31ca16b
turtleflyer <turtlefly@gmail.com>
2019-02-26 21:11:41 -0500

    Add filter for choosing faucets in level editor

--- src/debug/levelEditor/faucetsRelatedForm.js
+++ src/debug/levelEditor/faucetsRelatedForm.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 import './style.css';
-import { faucetModels } from '../../screens/gameplay/role_sets/faucets/faucets_params';
+import { faucetModels, switchTypes } from '../../screens/gameplay/role_sets/faucets/faucets_params';
 import { formTypes } from './formSet';
 
 function createCheckBox(name) {
@@ -12,27 +12,35 @@ function createCheckBox(name) {
   return checkBox;
 }
 
-export const faucetModelKey = Symbol('faucetModelKey');
-
 export default function faucetsRelatedForm(valueKeeper) {
-  const checkBoxes = [];
+  const checkBoxesMap = new Map();
   const form = document.createElement('div');
   form.className = 'level-form';
   Object.entries(faucetModels).forEach(([k, f]) => {
     const cb = createCheckBox(k);
     cb.checked = valueKeeper.value.includes(f);
-    cb[faucetModelKey] = k;
-    checkBoxes.push(cb);
+    checkBoxesMap.set(k, cb);
     form.appendChild(cb);
     form.appendChild(document.createTextNode(`${cb.name}\u00A0\u00A0`));
   });
 
   return {
     form,
-    checkBoxes,
+    checkBoxes: checkBoxesMap.values(),
     checkState(key, check = true) {
       if (check) {
-        valueKeeper.value.push(faucetModels[key]);
+        const { value: recentValue } = valueKeeper;
+        const faucetToAdd = faucetModels[key];
+        if (
+          recentValue.length === 3
+          || (recentValue.length === 2
+            && (faucetToAdd.switchType === switchTypes.DUAL
+              || recentValue.filter(f => f.switchType === switchTypes.DUAL).length >= 1))
+        ) {
+          checkBoxesMap.get(key).checked = false;
+        } else {
+          recentValue.push(faucetToAdd);
+        }
       } else {
         valueKeeper.value = valueKeeper.value.filter(f => f !== faucetModels[key]);
       }
