5e0105c88cf4199cef22b1f7219583b294916b38
turtleflyer <turtlefly@gmail.com>
2019-03-03 00:02:44 -0500

    Rename and structurize levelEditor module

--- src/debug/levelEditor/levelEditor.js
+++ src/debug/levelEditor/levelEditor.js
@@ -10,89 +10,102 @@ import { faucetModels } from '../../screens/gameplay/role_sets/faucets/faucets_p
 const levelEditor = document.createElement('div');
 levelEditor.className = 'tab-panel';
 
-let state;
+const state = {};
+
+const copyButtonWrapper = document.createElement('div');
+copyButtonWrapper.className = 'copy-alert-text';
+const copyParamsButton = document.createElement('button');
+copyParamsButton.className = 'debug-panel-button';
+copyParamsButton.innerText = 'Copy params';
+copyButtonWrapper.appendChild(copyParamsButton);
+
+copyParamsButton.addEventListener('click', () => {
+  const descriptionToCopy = revertToOriginalStructure(state.levelEdit);
+  const {
+    params: {
+      faucetsDescription: { models },
+      mugsDistribution,
+    },
+  } = descriptionToCopy;
+  descriptionToCopy.params.faucetsDescription.models = models.map(
+    f => `faucetModels.${Object.entries(faucetModels).filter(entr => f === entr[1])[0][0]}`,
+  );
+
+  descriptionToCopy.params.mugsDistribution = Object.entries(mugsDistribution).reduce(
+    (r, [k, v]) => {
+      r[`[${Object.entries(mugsTypes).filter(entr => k === entr[1])[0][0]}]`] = v;
+      return r;
+    },
+    {},
+  );
+
+  const stringToCopy = JSON.stringify(descriptionToCopy, null, 2).replace(/"/g, '');
+  navigator.clipboard
+    .writeText(stringToCopy)
+    .then(() => {
+      const text = document.createTextNode('Parameters copied');
+      copyButtonWrapper.appendChild(text);
+      return new Promise((resolve) => {
+        window.setTimeout(() => resolve(text), 2000);
+      });
+    })
+    .then(text => text.remove());
+});
 
 export function initializeLevelEditor(levelDescription) {
   levelEditor.innerHTML = '';
-  state = createObjStructureImage(levelDescription);
+  state.levelEdit = createObjStructureImage(levelDescription);
 
-  const { params: levelParams, initState } = state;
+  const {
+    levelEdit: {
+      params: {
+        mugsSpeed,
+        initMugDensity,
+        remainingTime,
+        moneyToEarn,
+        mugsDistribution,
+        faucetsDescription: { models: faucets },
+      },
+      initState: { money: initMoney },
+    },
+  } = state;
 
-  const mugSpeedInput = formToAcceptValue('Mugs Speed', levelParams.mugsSpeed, 'value');
-  levelEditor.appendChild(mugSpeedInput.form);
+  const mugSpeedInputForm = formToAcceptValue('Mugs Speed', mugsSpeed, 'value');
+  levelEditor.appendChild(mugSpeedInputForm.form);
 
-  const initMugDensity = formToAcceptValue('Init Mug Density', levelParams.initMugDensity, 'value');
-  levelEditor.appendChild(initMugDensity.form);
+  const initMugDensityForm = formToAcceptValue('Init Mug Density', initMugDensity, 'value');
+  levelEditor.appendChild(initMugDensityForm.form);
 
-  const remainingTime = formToAcceptValue('Remaining Time', levelParams.remainingTime, 'value');
-  levelEditor.appendChild(remainingTime.form);
+  const remainingTimeForm = formToAcceptValue('Remaining Time', remainingTime, 'value');
+  levelEditor.appendChild(remainingTimeForm.form);
 
-  const moneyToEarn = formToAcceptValue('Money To Earn', levelParams.moneyToEarn, 'value');
-  levelEditor.appendChild(moneyToEarn.form);
+  const moneyToEarnForm = formToAcceptValue('Money To Earn', moneyToEarn, 'value');
+  levelEditor.appendChild(moneyToEarnForm.form);
 
-  const initMoney = formToAcceptValue('Money in Beginning', initState.money, 'value');
-  levelEditor.appendChild(initMoney.form);
+  const initMoneyForm = formToAcceptValue('Money in Beginning', initMoney, 'value');
+  levelEditor.appendChild(initMoneyForm.form);
   levelEditor.appendChild(document.createElement('br'));
 
-  const faucetsCheckBoxes = faucetsRelatedForm(levelParams.faucetsDescription.models);
-  levelEditor.appendChild(faucetsCheckBoxes.form);
+  const faucetsCheckBoxesForm = faucetsRelatedForm(faucets);
+  levelEditor.appendChild(faucetsCheckBoxesForm.form);
   levelEditor.appendChild(document.createElement('br'));
 
-  const mugsDistr = mugsDistributionForm(levelParams.mugsDistribution);
-  levelEditor.appendChild(mugsDistr.form);
+  const mugsDistrForm = mugsDistributionForm(mugsDistribution);
+  levelEditor.appendChild(mugsDistrForm.form);
   levelEditor.appendChild(document.createElement('br'));
+
+  levelEditor.appendChild(copyButtonWrapper);
 
   formsSet.clearElements();
   formsSet.addElements([
-    mugSpeedInput,
-    initMugDensity,
-    remainingTime,
-    moneyToEarn,
-    initMoney,
-    faucetsCheckBoxes,
+    mugSpeedInputForm,
+    initMugDensityForm,
+    remainingTimeForm,
+    moneyToEarnForm,
+    initMoneyForm,
+    faucetsCheckBoxesForm,
   ]);
-  formsSet.addElements(mugsDistr.mugForms);
-
-  const copyButtonWrapper = document.createElement('div');
-  copyButtonWrapper.className = 'copy-alert-text';
-  const copyParamsButton = document.createElement('button');
-  copyParamsButton.className = 'debug-panel-button';
-  copyParamsButton.innerText = 'Copy params';
-  levelEditor.appendChild(copyButtonWrapper);
-  copyButtonWrapper.appendChild(copyParamsButton);
-
-  copyParamsButton.addEventListener('click', () => {
-    const descriptionToCopy = revertToOriginalStructure(state);
-    const {
-      params: {
-        faucetsDescription: { models },
-        mugsDistribution,
-      },
-    } = descriptionToCopy;
-    descriptionToCopy.params.faucetsDescription.models = models.map(
-      f => `faucetModels.${Object.entries(faucetModels).filter(entr => f === entr[1])[0][0]}`,
-    );
-
-    descriptionToCopy.params.mugsDistribution = Object.entries(mugsDistribution).reduce(
-      (r, [k, v]) => {
-        r[`[${Object.entries(mugsTypes).filter(entr => k === entr[1])[0][0]}]`] = v;
-        return r;
-      },
-      {},
-    );
-
-    const stringToCopy = JSON.stringify(descriptionToCopy, null, 2).replace(/"/g, '');
-    navigator.clipboard
-      .writeText(stringToCopy)
-      .then(() => {
-        const text = document.createTextNode('Parameters copied');
-        copyButtonWrapper.appendChild(text);
-        return new Promise((resolve) => {
-          window.setTimeout(() => resolve(text), 2000);
-        });
-      })
-      .then(text => text.remove());
-  });
+  formsSet.addElements(mugsDistrForm.mugForms);
 }
 
 export default levelEditor;
