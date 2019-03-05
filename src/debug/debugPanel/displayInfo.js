e745ace1179fff187608aaac74c3107c80fb6358
turtleflyer <turtlefly@gmail.com>
2019-03-04 22:25:35 -0500

    Add ability to set precise and to filter displayed values

--- src/debug/debugPanel/displayInfo.js
+++ src/debug/debugPanel/displayInfo.js
@@ -4,6 +4,9 @@ import stage from '../../stage/stage';
 import { fillingMugs } from '../../screens/gameplay/role_sets/fillingMugs/fillingMugs';
 import { setD, updateDebugInfo } from './setD';
 
+const filteredKeysToDisplay = ['reputation', 'drunkFactor', 'lifeTime', 'remainingTime'];
+const precise = 1;
+
 const displayInfo = document.createElement('div');
 
 export default displayInfo;
@@ -16,13 +19,19 @@ updateDebugInfo
   .registerAction(infoSet, {
     action() {
       const textToDisplay = `stage.state: ${JSON.stringify(
-        {
+        Object.entries({
           ...stage.state,
           remainingTime: stage.params.levelParams.remainingTime,
-        },
+        }).reduce((obj, [k, v]) => {
+          if (filteredKeysToDisplay.includes(k)) {
+            return { ...obj, [k]: v };
+          }
+          return { ...obj };
+        }, {}),
         (_, value) => {
           if (typeof value === 'number') {
-            return Math.round(value * 1000) / 1000;
+            // prettier-ignore
+            return (Math.round(value * (10 ** precise)) / (10 ** precise)).toFixed(precise);
           }
           return value;
         },
