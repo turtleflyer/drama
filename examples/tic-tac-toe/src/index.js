224c1647b218ad91a0defe7ab05850a8fb870904
turtleflyer <turtlefly@gmail.com>
2019-03-21 01:35:55 -0400

    Move analyzePossibleWin to separate file

--- examples/tic-tac-toe/src/index.js
+++ examples/tic-tac-toe/src/index.js
@@ -8,6 +8,7 @@ import {
 } from './cell';
 import postTextMessage from './messages';
 import './stage';
+import analyzePossibleWin from './analyzePossibleWin';
 import restartButton from './restartButton';
 
 const turns = ['setX', 'setO'];
@@ -36,34 +37,6 @@ emptyCells.addElements(gameMatrix);
 function keepState(cell, filledCells) {
   const cellN = gameMatrix.indexOf(cell);
   return [...filledCells, cellN].sort((a, b) => a - b);
-}
-
-function analyzePossibleWin(filledCells) {
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (const pattern of winPatterns) {
-    let winCells = [];
-    for (const cellN of pattern) {
-      if (filledCells.includes(cellN)) {
-        winCells.push(cellN);
-      } else {
-        winCells = [];
-        break;
-      }
-    }
-    if (winCells.length > 0) {
-      return winCells;
-    }
-  }
-  return false;
 }
 
 const cellUsedMessage = 'The cell is already set';
