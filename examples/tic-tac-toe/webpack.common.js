0b6cc37541c84b77658e8a360821c3b576580147
turtleflyer <turtlefly@gmail.com>
2019-03-25 19:45:45 -0400

    Change webpack production settings

--- examples/tic-tac-toe/webpack.common.js
+++ examples/tic-tac-toe/webpack.common.js
@@ -7,7 +7,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    ttt: './src/index.js',
   },
   module: {
     rules: [
@@ -40,7 +40,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../../dist/ttt'),
     globalObject: 'this',
   },
 };
