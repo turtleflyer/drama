626c806c6fda23d862903c68535b07a6a3c3bc5f
Andrei Kovalev <turtlefly@gmail.com>
2019-04-22 00:41:28 -0400

    Change title and description of page

--- packages/drama-page/gatsby-config.js
+++ packages/drama-page/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Drama',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+      'The page provides the documentation of the library and ',
+    author: '@turtleflyer',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
