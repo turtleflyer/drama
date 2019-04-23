bd6a77f34f638a2b45c8457b3a354e780e734786
Andrei Kovalev <turtlefly@gmail.com>
2019-04-23 06:25:35 -0400

    Add grommet

--- packages/drama-page/src/components/layout.js
+++ packages/drama-page/src/components/layout.js
@@ -9,6 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
+import { ResponsiveContext, Grommet } from 'grommet';
 import Header from './header';
 import './layout.css';
 import SectionsSideBar from './SectionsSideBar';
@@ -25,26 +26,32 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          <SectionsSideBar />
-          <main>{children}</main>
-          <footer>
-            {'©'}
-            {new Date().getFullYear()}
-            {', Built with '}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <Grommet>
+        <ResponsiveContext.Consumer>
+          {size => (
+            <>
+              <Header siteTitle={data.site.siteMetadata.title} />
+              <div
+                style={{
+                  margin: '0 auto',
+                  maxWidth: 960,
+                  padding: '0px 1.0875rem 1.45rem',
+                  paddingTop: 0,
+                }}
+              >
+                <SectionsSideBar />
+                <main>{children}</main>
+                <footer>
+                  {'©'}
+                  {new Date().getFullYear()}
+                  {', Built with '}
+                  <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </footer>
+              </div>
+            </>
+          )}
+        </ResponsiveContext.Consumer>
+      </Grommet>
     )}
   />
 );
