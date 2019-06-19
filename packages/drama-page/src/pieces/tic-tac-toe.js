a9a839d1ae8203f098d06fe5e27112408d6f936c
Andrei Kovalev <turtlefly@gmail.com>
2019-06-19 01:26:01 -0400

    Fix document undefined error while compiling for deplyment

--- packages/drama-page/src/pieces/tic-tac-toe.js
+++ packages/drama-page/src/pieces/tic-tac-toe.js
@@ -1,6 +1,7 @@
-import ticTacToe from '@drama-examples/tic-tac-toe';
-import replaceDOM from '../utils/replaceDOM';
-
-const TicTacToe = replaceDOM(ticTacToe);
-
-export default TicTacToe;
+if (typeof document !== 'undefined') {
+  const { default: replaceDOM } = module.require('../utils/replaceDOM');
+  const { default: ticTacToe } = module.require('@drama-examples/tic-tac-toe');
+  module.exports = replaceDOM(ticTacToe);
+} else {
+  module.exports = () => null;
+}
