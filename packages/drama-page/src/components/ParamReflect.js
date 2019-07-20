56cf6e07c5537b99670577afdc93c84de4778b31
Andrei Kovalev <turtlefly@gmail.com>
2019-07-20 01:39:52 -0400

    Add ScrollReflect component

---
+++ packages/drama-page/src/components/ParamReflect.js
@@ -0,0 +1,39 @@
+import React, { Children, cloneElement } from 'react';
+import PropTypes from 'prop-types';
+
+const continuosRange = Symbol('continuous range');
+
+const pickInRange = (range, point) => {
+  const { length } = range;
+  if (length === 3 && range[1] === continuosRange) {
+    return range[0] + (range[2] - range[0]) * point;
+  }
+
+  return range[Math.round((length - 1) * point)];
+};
+
+const ParamReflect = ({ mappedProps, point, children }) => {
+  const actualProps = Object.keys(mappedProps).reduce(
+    (props, key) => ({ ...props, [key]: pickInRange(mappedProps[key], point) }),
+    {},
+  );
+
+  return (
+    <>
+      {Children.map(children, (el) => {
+        if (el) {
+          return cloneElement(el, { ...actualProps });
+        }
+      })}
+    </>
+  );
+};
+
+ParamReflect.propTypes = {
+  mappedProps: PropTypes.objectOf(PropTypes.array.isRequired).isRequired,
+  point: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default ParamReflect;
+export { continuosRange };
